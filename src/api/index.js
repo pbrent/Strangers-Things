5099b699adb01b462c42af3801dd95df277a3953
Patrick <pbrent99@gmail.com>
2022-07-07 18:59:22 -0500

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+
+
+export const baseUrl = 'https://strangers-things.herokuapp.com/api/2206-FTB-ET-WEB-FT-B'
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import   ReactDOM   from "react-dom";
+
+
+const App = () => {
+
+    return (
+        
+        <h1>Placeholder</h1>
+    );
+};
+
+let appElement = document.getElementById("app");
+ReactDOM.render(<App />, appElement); 
