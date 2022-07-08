6ba13b719d1232a373916a1b4061f5a721817f5f
Patrick <pbrent99@gmail.com>
2022-07-07 22:52:13 -0500

    trying to get posts to render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-
+import { baseUrl } from "/root/Code/Strangers-Things/src/api/index.js";
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+//   console.log("this is posts: ", posts);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`${baseUrl}/posts`);
+      const result = await response.json();
+
+      setPosts(result);
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
-
-    <h1>Placeholder</h1>
+    <>
+      <h1>Placeholder</h1>
+      {
+        posts.map((post) => <div key={post.data.posts[0].id}>
+            <h3>{post.title}</h3>
+            <div>{post.body}</div>
+        
+        </div>)
+      }
+    </>
   );
 };
 
