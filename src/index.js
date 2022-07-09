2beb4c75ed18101755426877d423c41925aa7521
pbrent <pbrent99@gmail.com>
2022-07-09 17:45:41 -0500

    still can't get posts to render

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { baseUrl } from "/root/Code/Strangers-Things/src/api/index.js";
+// import {baseUrl} from './api';
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-//   console.log("this is posts: ", posts);
+  const [allPosts, setAllPosts] = useState([]);
+  // console.log(allPosts)
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`${baseUrl}/posts`);
-      const result = await response.json();
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await fetch(`${baseUrl}/posts`);
+  //       const result = await response.json();
+        
+        
+  //       setAllPosts(result);
+        
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
 
-      setPosts(result);
-    };
+  //   };
 
-    fetchPosts();
-  }, []);
+  //   fetchPosts();
+  // }, []);
+
+    useEffect(() => {
+      const fetchPosts = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        const result = await response.json();
+        
+        setAllPosts(result);
+      }
+      fetchPosts();
+    })
 
   return (
-    <>
-      <h1>Placeholder</h1>
-      {/* {
-        posts.map((post) => <div key={post.data.posts[0].id}>
-            <h3>{post.title}</h3>
-            <div>{post.body}</div>
-        
-        </div>
-      )} */}
-    </>
+    <div>
+      <h1>POSTS</h1>
+      
+    </div>
   );
 };
 
